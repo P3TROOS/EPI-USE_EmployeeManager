ddca35a7fa1fe67fad14f12bb738e0aca57bfe21
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-11-03 21:28:42 +0200

    Implemented Frontend login functionality

--- frontend_employeemanager/src/components/login.js
+++ frontend_employeemanager/src/components/login.js
@@ -6,34 +6,39 @@ const Login = () => {
     const[email,setEmail]=useState("")
     const[password,setPassword]=useState("")
 
-    // const handleLogin = (e) => {
-    //     e.preventDefault();
-    //     // Perform login authentication here
-    //     console.log('Username:', username);
-    //     console.log('Password:', password);
-    //     // You can add your authentication logic here
-    // };
-
-    const handleLogin = (e) => {
-        e.preventDefault()
-        const login = {email, password}
-        fetch("http://localhost:8080/api/auth/login", {
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body:JSON.stringify(login)
-        }).then((response) => {
-            console.log(response);
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        // Storing user login details for request
+        const credentials = {
+            email: email,
+            password: password,
+        };
+        try {
+            // Backend api login call with user details
+            const response = await fetch('http://localhost:8080/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(credentials),
+            });
             if (response.ok) {
+                // If login successful, redirect to homepage
                 response.json().then((data) => {
                     const { access_token } = data;
                     sessionStorage.setItem("accessToken", access_token);
-                    console.log("Login successful!");
+                    console.log("Login successful");
                     window.location.href = "/home";
                 });
             } else {
+                // If login unsuccessful, alert user
+                console.log('Login unsuccessful');
                 window.alert("Incorrect Login Details");
             }
-        })
+        } catch (error) {
+            // Handle error
+            console.error('Error occurred:', error);
+        }
     };
 
     return (
@@ -66,7 +71,7 @@ const Login = () => {
                                         required
                                     />
                                 </div>
-                                <button type="submit" className="btn btn-primary btn-block" onClick={handleLogin}>Login</button>
+                                <button type="submit" className="btn btn-primary btn-block">Login</button>
                             </form>
                         </div>
                     </div>
