76492538678e2e21db89e90317e0aa7dec095c83
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-11-03 19:44:27 +0200

    Added login function

--- frontend_employeemanager/src/components/login.js
+++ frontend_employeemanager/src/components/login.js
@@ -3,15 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styling/login.css';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+
+    // const handleLogin = (e) => {
+    //     e.preventDefault();
+    //     // Perform login authentication here
+    //     console.log('Username:', username);
+    //     console.log('Password:', password);
+    //     // You can add your authentication logic here
+    // };
 
     const handleLogin = (e) => {
-        e.preventDefault();
-        // Perform login authentication here
-        console.log('Username:', username);
-        console.log('Password:', password);
-        // You can add your authentication logic here
+        e.preventDefault()
+        const login = {email, password}
+        fetch("http://localhost:8080/api/auth/login", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(login)
+        }).then((response) => {
+            console.log(response);
+            if (response.ok) {
+                response.json().then((data) => {
+                    const { access_token } = data;
+                    sessionStorage.setItem("accessToken", access_token);
+                    console.log("Login successful!");
+                    window.location.href = "/home";
+                });
+            } else {
+                window.alert("Incorrect Login Details");
+            }
+        })
     };
 
     return (
@@ -23,18 +45,18 @@ const Login = () => {
                             <h3 className="card-title text-center">Login</h3>
                             <form onSubmit={handleLogin}>
                                 <div className="form-group">
-                                    <label htmlFor="username">Username</label>
+                                    <label htmlFor="username">User Email</label>
                                     <input
                                         type="text"
                                         className="form-control"
                                         id="username"
-                                        value={username}
-                                        onChange={(e) => setUsername(e.target.value)}
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
                                         required
                                     />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="password">Password</label>
+                                    <label htmlFor="password">User Password</label>
                                     <input
                                         type="password"
                                         className="form-control"
@@ -44,7 +66,7 @@ const Login = () => {
                                         required
                                     />
                                 </div>
-                                <button type="submit" className="btn btn-primary btn-block">Login</button>
+                                <button type="submit" className="btn btn-primary btn-block" onClick={handleLogin}>Login</button>
                             </form>
                         </div>
                     </div>
--- frontend_employeemanager/src/index.js
+++ frontend_employeemanager/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./components/login";
+import Home from "./components/home"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,7 +12,7 @@ root.render(
     <BrowserRouter>
         <Routes>
             <Route index element={<Login />} />
-            {/*<Route path='/Home' element={<Home />} />*/}
+            <Route path='/Home' element={<Home />} />
         </Routes>
     </BrowserRouter>
 );
